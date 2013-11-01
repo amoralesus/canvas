7d90ef46468aa24d53b9fe50b967a8c17bbd660a
Hashrocket Workstation <dev@hashrocket.com>
2013-11-01 13:18:52 -0400

    something about a secret_key deprecation; added to applicaiton.rb

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module Canvas
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
+    config.secret_key_base = 'new_error_showing_up'
+    
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
